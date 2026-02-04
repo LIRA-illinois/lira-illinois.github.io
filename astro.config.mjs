9d977d2126d54972fe830c7a32f8f9f96db1f533
Huy T. Tran <huytran1@illinois.edu>
2026-02-04 16:16:01 -0600

    add social icons using astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lira-illinois.github.io',
-	integrations: [mdx(), sitemap()],
+    site: 'https://lira-illinois.github.io',
+    integrations: [mdx(), sitemap(), icon()],
 });
