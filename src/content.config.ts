5d3d1b3ff482c845cda2f9d6c9b10e91d696be99
Huy T. Tran <huytran1@illinois.edu>
2026-04-02 12:19:02 -0500

    delete blog files

--- src/content.config.ts
+++ src/content.config.ts
@@ -76,20 +76,5 @@ const team = defineCollection({
   }),
 });
 
-// const blog = defineCollection({
-//   // Load Markdown and MDX files in the `src/content/blog/` directory.
-//   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-//   // Type-check frontmatter using a schema
-//   schema: ({ image }) =>
-//     z.object({
-//       title: z.string(),
-//       description: z.string(),
-//       // Transform string to Date object
-//       pubDate: z.coerce.date(),
-//       updatedDate: z.coerce.date().optional(),
-//       heroImage: image().optional(),
-//     }),
-// });
-
 // Export a single `collections` object to register your collection(s)
 export const collections = { news, papers, projects, team };
